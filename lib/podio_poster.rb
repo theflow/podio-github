c231e702e7c8a8357a2928c1d93cf9f181293dd3
Florian Munz <surf@theflow.de>
2013-07-26 14:49:29 +0200

    handle category and state fields

--- lib/podio_poster.rb
+++ lib/podio_poster.rb
@@ -27,11 +27,12 @@ module Podio
     end
 
     def update_item_on_podio(item_id, cmd, comment)
-      return unless ticket_does_exist?(item_id)
+      item = get_item(item_id)
+      return if item.nil?
       return unless [:cmd_close, :cmd_ref].include?(cmd)
 
       add_comment_to_item(item_id, comment)
-      set_status_to_fixed(item_id) if cmd == :cmd_close
+      set_status_to_fixed(item) if cmd == :cmd_close
     end
 
     def add_comment_to_item(item_id, comment)
@@ -41,20 +42,28 @@ module Podio
       end
     end
 
-    def set_status_to_fixed(item_id)
-      fields = []
-      fields << {:external_id => 'status', :values => [{'value' => 'Fixed'}]}
+    def set_status_to_fixed(item)
+      status_field = item['fields'].find { |field| field['external_id'] == 'status' && field['status'] =='active' }
+      return if status_field.nil?
+
+      if status_field['type'] == 'category'
+        option_id = status_field['config']['settings']['options'].find { |option| option['text'] == 'Fixed' }['id']
+        fields = [{:external_id => 'status', :values => [{'value' => option_id}]} ]
+      else
+        # old status field
+        fields = [{:external_id => 'status', :values => [{'value' => 'Fixed'}]} ]
+      end
 
       @podio_client.connection.put do |req|
-        req.url "/item/#{item_id}/value"
+        req.url "/item/#{item['item_id']}/value"
         req.body = fields
       end
     end
 
-    def ticket_does_exist?(ticket_id)
-      @podio_client.connection.get("/item/#{ticket_id}").status == 200
-    rescue Podio::AuthorizationError
-      false
+    def get_item(ticket_id)
+      @podio_client.connection.get("/item/#{ticket_id}").body
+    rescue Podio::AuthorizationError, Podio::GoneError, Podio::NotFoundError
+      nil
     end
   end
 end
